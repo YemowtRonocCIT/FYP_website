dcb2f81932d598199ff051ca3d92f992084f483f
Conor Twomey <conor.twomey1@mycit.ie>
2018-04-04 15:56:28 +0100

    Separate constants from code

--- app.py
+++ app.py
@@ -9,21 +9,28 @@ app = Flask(__name__)
 API_IP_ADDRESS = '127.0.0.1:5000'
 HOST_IP_ADDRESS = '192.168.153.1'
 
+NODE_SUFFIX = '/node/'
+
+HTTP_PREFIX = 'http://'
+
+SIGFOX_ID_KEY = 'sigfox_id'
 
 @app.route('/')
 def index_page():
     return "Hello, World!"
 
-@app.route('/node/', methods=['GET', 'POST'])
+@app.route(NODE_SUFFIX, methods=['GET', 'POST'])
 def nodes_page():
     if request.method == 'POST':
-        sigfox_id = request.form.get('sigfox_id')
-        data = {'sigfox_id': sigfox_id}
-        requests.post('http://%s/node/' % (API_IP_ADDRESS), data)
+        sigfox_id = request.form.get(SIGFOX_ID_KEY)
+        data = {SIGFOX_ID_KEY: sigfox_id}
+        requests.post('%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, NODE_SUFFIX), data)
     
-    response = requests.get('http://%s/node/' % (API_IP_ADDRESS))
+    response = requests.get('%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, NODE_SUFFIX))
     data = json.loads(response.text)
     return render_template('nodes_table.html', nodes=data, host_ip=HOST_IP_ADDRESS)
+
+@app.route()
 
 if __name__ == '__main__':
     app.run(host=HOST_IP_ADDRESS)
