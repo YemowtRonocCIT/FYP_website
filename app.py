e980c9986c9b07a66dd3cd83d38d192a25be9991
Conor Twomey <conor.twomey1@mycit.ie>
2018-04-04 14:20:28 +0100

    Separate IP Addresses from code

--- app.py
+++ app.py
@@ -6,21 +6,24 @@ from flask import request
 
 app = Flask(__name__)
 
+API_IP_ADDRESS = '127.0.0.1:5000'
+HOST_IP_ADDRESS = '192.168.153.1'
+
 
 @app.route('/')
 def index_page():
     return "Hello, World!"
 
-@app.route('/nodes', methods=['GET', 'POST'])
+@app.route('/node/', methods=['GET', 'POST'])
 def nodes_page():
     if request.method == 'POST':
         sigfox_id = request.form.get('sigfox_id')
         data = {'sigfox_id': sigfox_id}
-        requests.post('http://192.168.153.1:5000/node/', data)
+        requests.post('http://%s/node/' % (API_IP_ADDRESS), data)
     
-    response = requests.get('http://192.168.153.1:5000/node')
+    response = requests.get('http://%s/node/' % (API_IP_ADDRESS))
     data = json.loads(response.text)
-    return render_template('nodes_table.html', nodes=data)
+    return render_template('nodes_table.html', nodes=data, host_ip=HOST_IP_ADDRESS)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=HOST_IP_ADDRESS)
