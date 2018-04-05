aaca07adc700b364dd509a17323f05a9fa5c27ee
Conor Twomey <conor.twomey1@mycit.ie>
2018-04-05 12:54:17 +0100

    Change recalculated value to variable

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ def index_page():
 
 @app.route(NODE_SUFFIX, methods=['GET', 'POST'])
 def nodes_page():
+    url = '%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, NODE_SUFFIX)
     if request.method == 'POST':
         sigfox_id = request.form.get(SIGFOX_ID_KEY)
         data = {SIGFOX_ID_KEY: sigfox_id}
-        requests.post('%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, NODE_SUFFIX), data)
+        requests.post(url, data)
     
-    response = requests.get('%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, NODE_SUFFIX))
+    response = requests.get(url)
     data = json.loads(response.text)
     return render_template('nodes_table.html', nodes=data, host_ip=HOST_IP_ADDRESS)
 
