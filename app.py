4f295e78b448924f444fff3e591c768d001488a5
Conor Twomey <conor.twomey1@mycit.ie>
2018-04-05 12:53:14 +0100

    Add locations form example to repository

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ API_IP_ADDRESS = '127.0.0.1:5000'
 HOST_IP_ADDRESS = '192.168.153.1'
 
 NODE_SUFFIX = '/node/'
+LOCATION_SUFFIX = '/location/'
 
 HTTP_PREFIX = 'http://'
 
 SIGFOX_ID_KEY = 'sigfox_id'
+LOCATION_NAME_KEY = 'location_name'
+LOCATION_TYPE_KEY = 'location_type'
 
 @app.route('/')
 def index_page():
@@ -30,7 +33,21 @@ def nodes_page():
     data = json.loads(response.text)
     return render_template('nodes_table.html', nodes=data, host_ip=HOST_IP_ADDRESS)
 
-@app.route()
+@app.route(LOCATION_SUFFIX, methods=['GET', 'POST'])
+def locations_pages():
+    url = '%s%s%s' % (HTTP_PREFIX, API_IP_ADDRESS, LOCATION_SUFFIX)
+    if request.method == 'POST':
+        location_name = request.form.get(LOCATION_NAME_KEY)
+        location_type = request.form.get(LOCATION_TYPE_KEY)
+        data = {
+            LOCATION_NAME_KEY: location_name,
+            LOCATION_TYPE_KEY: location_type
+        }
+        requests.post(url, data)
+    
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return render_template('locations_table.html', locations=data, host_ip=HOST_IP_ADDRESS)
 
 if __name__ == '__main__':
     app.run(host=HOST_IP_ADDRESS)
